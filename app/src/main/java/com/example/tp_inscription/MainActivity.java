b7f3fcc09edfcca68c8a6cfe873f4fdb332b60c5
Suranjan JAYASUNDRA <suranjan.jayasundra@gmail.fr>
2022-03-30 11:53:37 +0200

    Essay checkBox

--- app/src/main/java/com/example/tp_inscription/MainActivity.java
+++ app/src/main/java/com/example/tp_inscription/MainActivity.java
@@ -1,35 +1,39 @@
 package com.example.tp_inscription;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener{
 
     ResultSet rst = null;
     public static Connection conn = null;
+
     private EditText ajpseudo, ajmdp, ajcmdp, ajcommentaire;
     private Button ok;
-    private RadioButton lundi, mardi, mercredi, jeudi, vendredi, easy, medium, hard, selecteddiff, selectedjour;
+    private RadioButton  easy, medium, hard, selecteddiff, selectedjour;
     private RadioGroup radioGroup_diffLevel, radioGroup_jour;
+    private CheckBox lundi, mardi, mercredi, jeudi, vendredi;
     private Spinner spinner;
     private String pass, cpass;
 
@@ -37,7 +41,6 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test);
-
 
         StrictMode.setThreadPolicy(new
                 StrictMode.ThreadPolicy.Builder()
@@ -53,7 +56,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
                 .build());
 
         //appel de la connexion
-        MysqlConnexion();
+        mysqlConnexion();
 
 //Déclarations
         ajpseudo = (EditText) findViewById(R.id.ajpseudo);
@@ -64,11 +67,11 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         ok = (Button) findViewById(R.id.ok);
         ok.setOnClickListener(this);
 
-        lundi = (RadioButton) findViewById(R.id.lundi);
-        mardi = (RadioButton) findViewById(R.id.mardi);
-        mercredi = (RadioButton) findViewById(R.id.mercredi);
-        jeudi = (RadioButton) findViewById(R.id.jeudi);
-        vendredi = (RadioButton) findViewById(R.id.vendredi);
+        lundi = (CheckBox) findViewById(R.id.lundi);
+        mardi = (CheckBox) findViewById(R.id.mardi);
+        mercredi = (CheckBox) findViewById(R.id.mercredi);
+        jeudi = (CheckBox) findViewById(R.id.jeudi);
+        vendredi = (CheckBox) findViewById(R.id.vendredi);
         easy = (RadioButton) findViewById(R.id.easy);
         medium = (RadioButton) findViewById(R.id.medium);
         hard = (RadioButton) findViewById(R.id.hard);
@@ -76,44 +79,20 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         spinner = (Spinner) findViewById(R.id.spinner);
 
         radioGroup_diffLevel = (RadioGroup) findViewById(R.id.radioGroup_diffLevel);
-        radioGroup_jour = (RadioGroup) findViewById(R.id.radioGroup_jour);
 
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String name = spinner.getSelectedItem().toString();
-                Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
+        FillSpinner();
     }
 
-    private void MysqlConnexion() {
-        String jdbcURL = "jdbc:mysql://192.168.1.20:3306/inscription";
-        String user = "username";
-        String passwd = "password";
+    @SuppressLint("NewApi")
+    public void mysqlConnexion () {
+        String jdbcURL = "jdbc:mysql://10.4.253.123:3306/inscription";
+        String user = "monty";
+        String passwd = "some_pass";
 
         try {
             Class.forName("com.mysql.jdbc.Driver");
             conn = DriverManager.getConnection(jdbcURL, user, passwd);
             Toast.makeText(MainActivity.this, "Connexion reussie.", Toast.LENGTH_LONG).show();
-
-            Statement stmt = conn.createStatement();
-            String sql = "SELECT * FROM association";
-            ResultSet res = stmt.executeQuery(sql);
-
-            ArrayList<String> data = new ArrayList<String>();
-            while (res.next()) {
-                String id = res.getString("listassociation");
-                data.add(id);
-            }
-            String[] array = data.toArray(new String[0]);
-            ArrayAdapter NoCoreAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, data);
-            spinner.setAdapter(NoCoreAdapter);
 
         } catch (ClassNotFoundException e) {
             Toast.makeText(MainActivity.this, "Driver manquant." + e.getMessage().toString(), Toast.LENGTH_LONG).show();
@@ -134,10 +113,10 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         selecteddiff  = (RadioButton) findViewById(radioGroup_diffLevel.getCheckedRadioButtonId());
         selecteddiff  = (RadioButton) findViewById(selecteddiffid);
 
-        //Avoir le bon bouton selectionner ( RadioGroup : Diponibilité de la semaine )
+        /*/Avoir le bon bouton selectionner ( RadioGroup : Diponibilité de la semaine )
         int selectedjourid = radioGroup_jour.getCheckedRadioButtonId();
         selectedjour = (RadioButton) findViewById(radioGroup_jour.getCheckedRadioButtonId());
-        selectedjour  = (RadioButton) findViewById(selectedjourid);
+        selectedjour  = (RadioButton) findViewById(selectedjourid);*/
 
         //Avoir le bon spinner selectionner
         String text = spinner.getSelectedItem().toString();
@@ -146,17 +125,29 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         String pass=ajmdp.getText().toString();
         String cpass=ajcmdp.getText().toString();
 
+        String valuesOfCheckBox = "";
+        if (lundi.isSelected()) {
+            valuesOfCheckBox += lundi.getText() + " ";
+        }
+        if (mardi.isSelected()) {
+            valuesOfCheckBox += mardi.getText() + " ";
+        }
+        if (mercredi.isSelected()) {
+            valuesOfCheckBox += mercredi.getText() + " ";
+        }
+        if (jeudi.isSelected()) {
+            valuesOfCheckBox += jeudi.getText() + " ";
+        }
+        if (vendredi.isSelected()) {
+            valuesOfCheckBox += vendredi.getText() + " ";
+        }
+
         if (!pass.equals(cpass)){
                   Toast.makeText(MainActivity.this, "Mot de passe non identique" , Toast.LENGTH_LONG).show();
               }
         else{
-            if (selecteddiffid  == -1 || selectedjourid == -1) {
-                if (selecteddiffid  == -1){
-                    Toast.makeText(MainActivity.this, "Veuillez choisir un niveau" , Toast.LENGTH_LONG).show();
-                }
-                else if (selectedjourid == -1){
-                    Toast.makeText(MainActivity.this, "Veuillez choisir un jour" , Toast.LENGTH_LONG).show();
-                }
+            if (selecteddiffid  == -1 ) {
+                Toast.makeText(MainActivity.this, "Veuillez choisir un niveau" , Toast.LENGTH_LONG).show();
             }
             else{
                 try {
@@ -166,7 +157,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
                     pstmins.setString(2, ajmdp.getText().toString());
                     pstmins.setString(3, ajcommentaire.getText().toString());
                     pstmins.setString(4, selecteddiff.getText().toString());
-                    pstmins.setString(5, selectedjour.getText().toString());
+                    pstmins.setString(5, valuesOfCheckBox);
                     pstmins.setString(6, spinner.getSelectedItem().toString());
 
                     pstmins.executeUpdate();
@@ -181,13 +172,31 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         }
     }//fin de méthode onclick
 
+    public void FillSpinner(){
+        try {
+            mysqlConnexion();
+            String query = "select * from listassociation";
+            PreparedStatement stmt = conn.prepareStatement(query);
+            ResultSet rs = stmt.executeQuery();
+
+            ArrayList<String> data = new ArrayList<String>();
+            while (rs.next()) {
+                String association = rs.getString("association");
+                data.add(association);
+            }
+            ArrayAdapter array = new ArrayAdapter(this, android.R.layout.simple_list_item_1, data);
+            spinner.setAdapter(array);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
     public void videTexte() {
         ajpseudo.setText("");
         ajmdp.setText("");
         ajcmdp.setText("");
         ajcommentaire.setText("");
         radioGroup_diffLevel.clearCheck();
-        radioGroup_jour.clearCheck();
+        Checkbox.setSelected(false);
     }
-
 }
